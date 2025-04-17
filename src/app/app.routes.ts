eeecc79c7f0c0e18f76a389eda6925ef43d7fa26
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-17 15:39:40 +0200

    add fonts, routes, not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'cronjobs',
+    pathMatch: 'full',
+  },
+  {
+    path: 'cronjobs',
+    loadComponent: () =>
+      import('./presentation/pages/cronjobs/cronjobs.component').then(
+        (m) => m.CronjobsComponent
+      ),
+  },
+  {
+    path: 'cronjobs/:id',
+    loadComponent: () =>
+      import('./presentation/pages/cronjob/cronjob.component').then(
+        (m) => m.CronjobComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+    pathMatch: 'full',
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./presentation/pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+];
