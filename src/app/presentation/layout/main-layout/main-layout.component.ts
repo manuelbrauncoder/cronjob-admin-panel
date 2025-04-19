0ba83cfbc88eee6ea7483853c487525eb8211d6d
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-19 19:53:07 +0200

    sidebar mobile: slide over

--- src/app/presentation/layout/main-layout/main-layout.component.ts
+++ src/app/presentation/layout/main-layout/main-layout.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { CommonModule } from '@angular/common';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent, RouterOutlet, SidebarComponent],
+  imports: [HeaderComponent, RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
 export class MainLayoutComponent {
-
+  uiService = inject(UiService);
 }
