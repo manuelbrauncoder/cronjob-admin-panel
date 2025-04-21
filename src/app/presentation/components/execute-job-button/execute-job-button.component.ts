1065c32e3f2a103e0c1e30641d4916155e7f04c0
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-21 13:02:48 +0200

    add fetch jobs to dashboard

--- src/app/presentation/components/execute-job-button/execute-job-button.component.ts
+++ src/app/presentation/components/execute-job-button/execute-job-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, model } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ExecuteCronJobUseCaseService } from '../../../application/use-cases/execute-cron-job-use-case.service';
 import { CronJobRepository } from '../../../domain/services/cronjob-repository';
 import { CronjobApiService } from '../../../infrastructure/services/cronjob-api.service';
@@ -19,7 +19,8 @@ import { SnackService } from '../../services/snack.service';
   ],
 })
 export class ExecuteJobButtonComponent {
-  @Input() cronJobKey: string = ''
+  @Input() cronJobKey: string = '';
+
   executeCronJobUseCase = inject(ExecuteCronJobUseCaseService);
   snack = inject(SnackService);
 
@@ -28,13 +29,19 @@ export class ExecuteJobButtonComponent {
     this.executeCronJobUseCase.execute({ key: this.cronJobKey }).subscribe({
       next: (response: HttpResponse<void>) => {
         if (response.status === 204) {
-          this.snack.presentSnack({err: false, message: 'Success'});
+          this.snack.presentSnack({ err: false, message: 'Success' });
         } else {
-          this.snack.presentSnack({err: true, message: `Error executing ${this.cronJobKey}`})
+          this.snack.presentSnack({
+            err: true,
+            message: `Error executing ${this.cronJobKey}`,
+          });
         }
       },
       error: (err) => {
-        this.snack.presentSnack({err: true, message: `Error executing ${this.cronJobKey}`})
+        this.snack.presentSnack({
+          err: true,
+          message: `Error: ${err}`,
+        });
       },
     });
   }
--- src/app/presentation/pages/dashboard/dashboard.component.ts
+++ src/app/presentation/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CronJob } from '../../../domain/models/cronjob.interface';
+import { GetCronjobsUseCaseService } from '../../../application/use-cases/get-cronjobs-use-case.service';
+import { CronJobRepository } from '../../../domain/services/cronjob-repository';
+import { CronjobApiService } from '../../../infrastructure/services/cronjob-api.service';
+import { HttpResponse } from '@angular/common/http';
+import { SnackService } from '../../services/snack.service';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  providers: [
+    GetCronjobsUseCaseService,
+    {
+      provide: CronJobRepository,
+      useClass: CronjobApiService,
+    },
+  ],
 })
-export class DashboardComponent {
-  
+export class DashboardComponent implements OnInit {
+  cronJobs: CronJob[] = [];
+  getCronJobsUseCase = inject(GetCronjobsUseCaseService);
+  snack = inject(SnackService);
+
+  ngOnInit(): void {
+    this.getCronJobs();
+  }
+
+  getCronJobs(): void {
+    this.getCronJobsUseCase.execute().subscribe({
+      next: (response: HttpResponse<CronJob[]>) => {
+        if (response.status === 200) {
+          this.cronJobs = response.body as CronJob[];
+        } else {
+          this.snack.presentSnack({
+            err: true,
+            message: `Error: ${response.status}`,
+          });
+        }
+      },
+      error: (err) => {
+        this.snack.presentSnack({ err: true, message: `Error: ${err}` });
+      },
+    });
+  }
 }
