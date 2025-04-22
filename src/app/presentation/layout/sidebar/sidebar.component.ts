c79681b60c529834590209cbd2543b3f69b39c50
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-22 20:09:07 +0200

    change routerLinkActive for cronjob/cronjobs to helper method

--- src/app/presentation/layout/sidebar/sidebar.component.ts
+++ src/app/presentation/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UiService } from '../../services/ui.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
@@ -19,6 +20,11 @@ export class SidebarComponent {
     if (window.innerWidth < 1000) {
       this.uiService.isSidebarPresented = false;
     }
+  }
+
+  isRouteActive(): boolean {
+    const current = this.router.url;
+    return ['/cronjob', '/cronjobs'].some(route => current.startsWith(route));
   }
 
 }
